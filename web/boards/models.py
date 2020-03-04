cdc59518a4e289e791819958fbad1d917e0c7335
jslmariano <jslmariano@gmail.com>
2020-03-04 11:10:35 +0800

    ORM Enhancements * Added new tests for BOARDS * Added new templates for topic posts and replies * New migration for topic.views ( though not sure it if it's good to add this as django may unsync migration structures  per local ) * Fixed missing avatar.svg * Fixed environment variables * Added new boards forms class * Enhanced topic view layout and display counts aggregates * Added new url for topics business process

--- web/boards/models.py
+++ web/boards/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import Truncator
 
 from django.contrib.auth.models import User
 
@@ -6,15 +7,30 @@ class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
+
 
 class Topic(models.Model):
+    def __str__(self):
+        return self.name
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='topics')
     starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics')
+    views = models.PositiveIntegerField(default=0)
 
 
 class Post(models.Model):
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
     message = models.TextField(max_length=4000)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
