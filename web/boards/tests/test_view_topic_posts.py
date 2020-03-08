d94dd9845662a00930a9c615167bb49df58e82fe
jslmariano <jslmariano@gmail.com>
2020-03-08 11:11:43 +0800

    Class Based Views * New tests runs * Converted some few layout to CBV * Implemented pagination * Implemented gravatar * Enhanced accounts view * Enhanced boards model
    Signed-off-by: jslmariano <jslmariano@gmail.com>

--- web/boards/tests/test_view_topic_posts.py
+++ web/boards/tests/test_view_topic_posts.py
@@ -3,21 +3,21 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 
 from ..models import Board, Post, Topic
-from ..views import topic_posts
+from ..views import PostListView
 
 
 class TopicPostsTests(TestCase):
     def setUp(self):
-        board = Board.objects.create(name='Django', description='Django board.')
-        user = User.objects.create_user(username='john', email='john@doe.com', password='123')
-        topic = Topic.objects.create(subject='Hello, world', board=board, starter=user)
-        Post.objects.create(message='Lorem ipsum dolor sit amet', topic=topic, created_by=user)
-        url = reverse('topic_posts', kwargs={'pk': board.pk, 'topic_pk': topic.pk})
+        self.board = Board.objects.create(name='Django', description='Django board.')
+        self.user = User.objects.create_user(username='john', email='john@doe.com', password='123')
+        self.topic = Topic.objects.create(subject='Hello, world', board=self.board, starter=self.user)
+        self.post = Post.objects.create(message='Lorem ipsum dolor sit amet', topic=self.topic, created_by=self.user)
+        url = reverse('topic_posts', kwargs={'pk': self.board.pk, 'topic_pk': self.topic.pk})
         self.response = self.client.get(url)
 
     def test_status_code(self):
         self.assertEquals(self.response.status_code, 200)
 
     def test_view_function(self):
-        view = resolve('/boards/1/topics/1/')
-        self.assertEquals(view.func, topic_posts)
+        view = resolve('/boards/{0}/topics/{1}/'.format(self.board.pk, self.topic.pk))
+        self.assertEquals(view.func.view_class, PostListView)
